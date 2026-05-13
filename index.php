9ad2f8c70ce82a803845d9a33a82e0f99812a2f7
Juan Manuel Garrido Portillo <juangpdev@gmail.com>
2026-05-13 22:39:38 +0200

    Primer commit: subida inicial del archivo PHP

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+// Archivo de prueba para la Actividad 1.3 - DAW
+$nombre = "Juanma";
+$fecha = date("d-m-Y");
+
+echo "<h1>Bienvenido al curso de DAW</h1>";
+echo "<p>Este proyecto está siendo controlado por Git.</p>";
+echo "<p>Usuario: <strong>$nombre</strong></p>";
+echo "<p>Fecha de hoy: $fecha</p>";
+?>
